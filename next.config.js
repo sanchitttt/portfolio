e3b1ff766f18b0cb67996bb89b3ddb6a346bd5cb
Sanchit <sanchittewari222@gmail.com>
2023-06-29 03:08:23 +0530

    github redirection fix try 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/(.*)',
+                destination: `https://www.sanchittewari.vercel.app/$1`,
+                permanent: true,
+            },
+        ];
+    },
+};
 
 module.exports = nextConfig
