d99bdd166d15316143e95b92db691c4a3e9d5596
Sanchit <sanchittewari222@gmail.com>
2023-06-29 14:40:23 +0530

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <ProjectsSection />
       {/* <MiniProjects /> */}
       <CertificationSection />
-      {/* <Footer /> */}
+      <Footer />
       <Analytics />
     </>
   )
